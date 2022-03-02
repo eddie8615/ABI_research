336dcddd92b3eb6238bf1a7624ab850d4201a773
Changhyun Kim <movementfor@gmail.com>
2022-03-02 02:07:43 +0000

    redo partitioning audio files and will make emotion dataframes

--- audio_crop.py
+++ audio_crop.py
@@ -1,42 +1,71 @@
 import json
 import os
 from pydub import AudioSegment
+import pandas as pd
+
+arouse_path = os.path.relpath('./MSP Data/Annotations/Annotations/Arousal/MSP-Conversation_0021_1_001.csv')
+dominance_path = os.path.relpath('./MSP Data/Annotations/Annotations/Dominance')
+valence_path = os.path.relpath('./MSP Data/Annotations/Annotations/Valence')
+train_path = os.path.relpath('./Data/train')
+validation_path = os.path.relpath('./Data/validation')
+test_path = os.path.relpath('./Data/test')
 
 
 def main():
     labels = file_labels()
-    audio_segment(labels)
+    data = pd.read_csv(arouse_path, header=8, names=['time', 'arousal'])
+    print(data.head())
+    # audio_partition()
+    # audio_segment(labels)
 
 
 def audio_segment(labels):
     path = os.path.relpath('./MSP Data/Time Labels/segments.json')
-    audio_path = os.path.relpath('./MSP Data/Audio')
-    train_path = os.path.relpath('./Data/train')
-    validation_path = os.path.relpath('./Data/validation')
-    test_path = os.path.relpath('./Data/test')
+    part_path = os.path.relpath('./Data/partition/')
+
     f = open(path, 'r')
     timing_data = json.load(f)
-    sliced_files = []
 
     for key in timing_data:
         start_time = timing_data[key]['Start_Time'] * 1000
         end_time = timing_data[key]['End_Time'] * 1000
-        save_path = ''
 
         if timing_data[key]['Conversation_Part'][0:21] in labels[0]:
             save_path = os.path.join(train_path, key+'.wav')
+            save_y = os.path.join(train_path, key+'.csv')
         elif timing_data[key]['Conversation_Part'][0:21] in labels[1]:
             save_path = os.path.join(validation_path, key+'.wav')
+            save_y = os.path.join(train_path, key + '.csv')
         else:
             save_path = os.path.join(test_path, key+'.wav')
+            save_y = os.path.join(train_path, key + '.csv')
 
-        audio_file = timing_data[key]['Conversation_Part'][0:21] + '.wav'
-        audio_file = os.path.join(audio_path, audio_file)
+        audio_file = timing_data[key]['Conversation_Part'] + '.wav'
+        audio_file = os.path.join(part_path, audio_file)
         file = AudioSegment.from_wav(audio_file)
         sliced = file[start_time:end_time]
         sliced.export(save_path, format="wav")
-        sliced_files.append(sliced)
-    print(len(sliced_files))
+        arouse = pd.read_csv()
+
+
+def audio_partition():
+    path = os.path.relpath('./MSP Data/Time Labels/conversation_parts.txt')
+    part_path = os.path.relpath('./Data/partition/')
+    audio_path = os.path.relpath('./MSP Data/Audio')
+
+    with open(path) as f:
+        lines = f.readlines()
+
+    for line in lines:
+        elem = line.strip().split(';')
+        file_name = elem[0]+'.wav'
+        save_path = os.path.join(part_path, file_name)
+        start_time = float(elem[1]) * 1000
+        end_time = float(elem[2]) * 1000
+        audio_file = os.path.join(audio_path, elem[0][0:21]+'.wav')
+        file = AudioSegment.from_wav(audio_file)
+        sliced = file[start_time:end_time]
+        sliced.export(save_path, format="wav")
 
 
 def file_labels():
--- transcribing.py
+++ transcribing.py
@@ -4,52 +4,43 @@ from google.cloud import speech
 import wave
 from pydub import AudioSegment
 
-sample_path = os.path.relpath('./Data/train/MSP-PODCAST_0021_0003.wav')
+audio_path = os.path.relpath('./Data/train/')
+save_path = os.path.relpath('./transcripts/train')
 bucket_name = 'msc_research'
-# os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/changhyun/workspace/ABI_research/config/google_services.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/changhyun/workspace/ABI_research/config/google_services.json"
+
 
 def main():
     # check_if_files_mono()
     # frame_rate_channel(sample_path)
     transcribing()
 
+
 def transcribing():
     client = speech.SpeechClient()
-    with io.open(sample_path, "rb") as audio_file:
-        content = audio_file.read()
-    audio = speech.RecognitionAudio(content=content)
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,
-        language_code="en-US",
-    )
+    responses = []
+    for file in os.listdir(audio_path):
+        path = os.path.join(audio_path, file)
+        with io.open(path, "rb") as audio_file:
+            content = audio_file.read()
+        audio = speech.RecognitionAudio(content=content)
+        config = speech.RecognitionConfig(
+            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=16000,
+            language_code="en-US",
+        )
+        try:
+            responses.append(client.recognize(config=config, audio=audio))
+        except:
+            print(path)
 
-    response = client.recognize(config=config, audio=audio)
-    for result in response.results:
-        print("Transcript: {}".format(result.alternatives[0].transcript))
-
-
-# def upload_blob(bucket_name, source_file_name, destination_blob_name):
-#     """Uploads a file to the bucket."""
-#     storage_client = storage.Client()
-#     bucket = storage_client.get_bucket(bucket_name)
-#     blob = bucket.blob(destination_blob_name)
-#
-#     blob.upload_from_filename(source_file_name)
-#
-#
-# def delete_blob(bucket_name, blob_name):
-#     """Deletes a blob from the bucket."""
-#     storage_client = storage.Client()
-#     bucket = storage_client.get_bucket(bucket_name)
-#     blob = bucket.blob(blob_name)
-#
-#     blob.delete()
+    for response in responses:
+        print("Transcript: {}".format(response.alternatives[0].transcript))
 
 
 # The API does not support stereo audio files so that we need to check the audio files are mono
 def check_if_files_mono():
-    sample = AudioSegment.from_wav(sample_path)
+    sample = AudioSegment.from_wav(audio_path)
     print(sample.channels)
 
 
